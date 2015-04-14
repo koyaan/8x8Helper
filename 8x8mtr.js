a2bd4b04906c99ee02c5fbbb2d729275c0759e59
Armin Weihbold <armin.weihbold@pulpmedia.at>
2015-04-14 19:46:08 +0200

    initial commit
    meteor + meteor:angular

---
+++ 8x8mtr.js
@@ -0,0 +1,25 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+
+    angular.module('mtrang',['angular-meteor']);
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
